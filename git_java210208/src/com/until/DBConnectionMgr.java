240beb27cb7e3b4b6010e3dba8c42acd9d4aa5dd
goodlucky1215 <goodlucky1215@naver.com>
2021-03-10 12:21:55 +0900

    게으른 인스턴스와 이른 인스턴스화 비교

--- git_java210208/src/com/until/DBConnectionMgr.java
+++ git_java210208/src/com/until/DBConnectionMgr.java
@@ -8,11 +8,15 @@ import java.sql.ResultSet;
 public class DBConnectionMgr {
 	private static final String _DRIVER = "oracle.jdbc.driver.OracleDriver";
 	//private static final String _URL    = "jdbc:oracle:thin:@172.30.1.17:1521:orcl11";
-	private static final String _URL    = "jdbc:oracle:thin:@192.168.0.35:1521:orcl11";
+	private static final String _URL    = "jdbc:oracle:thin:@192.168.0.47:1521:orcl11";
 	private static final String _USER   = "scott";
 	private static final String _PW     = "tiger";
-	static DBConnectionMgr dbMgr = null;
+	private static DBConnectionMgr dbMgr = null;
+	//이른 인스턴스화 eager
+	//private static DBConnectionMgr dbMgr = new DBConnectionMgr();
 	Connection con = null;
+	private DBConnectionMgr() {}
+	//게으른 인스턴스화 - 선언과 생성이 따로 쓰여졌을 때
 	public static DBConnectionMgr getInstance() {
 		if(dbMgr == null) {
 			dbMgr = new DBConnectionMgr();
@@ -23,6 +27,12 @@ public class DBConnectionMgr {
 		try {
 			Class.forName(_DRIVER);		
 			con = DriverManager.getConnection(_URL, _USER , _PW);
+			/* 트랜잭션처리
+			con.setAutoCommit(true);//켜둔다.
+			con.setAutoCommit(false);//꺼둔다.
+			con.commit();
+			con.rollback();
+			*/
 		} catch (ClassNotFoundException ce) {
 			System.out.println("드라이버 클래스를 찾을 수 없습니다.");
 		} catch(Exception e) {
